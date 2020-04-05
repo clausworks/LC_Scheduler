7d20d6910705b7dcb57a6872c9a696ed775c1a91
ClausWorks <clausworks@zoho.com>
2020-04-05 10:25:12 -0700

    Changed subject display
    First column now displays subject area.
    Second column displays numbers.

--- generators.py
+++ generators.py
@@ -13,25 +13,24 @@ def write_shift_subjects(ws, tutors):
 
     # Write header column (cell coordinates: row=i,col=j)
 
-    # Write data.
-    j = 2
+    # Write header at top.
+    j = 3 # Column index for current cell.
     for cur_day in Shift.DAYS:
         ws.cell(row=1, column=j).value = cur_day  # DAY
         for cur_shift in all_shifts:
             ws.cell(row=2, column=j).value = cur_shift  # Shift N
             j += 1
-    # for subj_area,nums in all_subjects.items():
-    #     ws.cell(row=1, column=j).value = subj_area  # XXXX
-    #     for cur_num in nums:
-    #         ws.cell(row=2, column=j).value = cur_num  # ###
-    #         j += 1
+
+    # Write header on left side.
     i = 3  # Row index for current subject
     for subj_area,nums in all_subjects.items():
+        ws.cell(row=i, column=1).value = subj_area
         for m,cur_num in enumerate(nums):
             if cur_num == 'None': cur_num = '(All)'
-            ws.cell(row=i, column=1).value = subj_area + ' ' + cur_num  # XXXX ###
+            ws.cell(row=i, column=2).value = cur_num
             i += 1
 
+    # Write data.
     for i,row in enumerate(ws.iter_rows()):
         for j in range(len(row)):
             for tutor in tutors:
@@ -42,7 +41,8 @@ def write_shift_subjects(ws, tutors):
                             # shifts to get the index of the current day.
                             if (i>1 and j>0):
                                 day_col = ((j+2) // len(all_shifts)) * len(all_shifts) - 1
-                                if (str(tutor_subj) == row[0].value
+                                if (tutor_subj.name == row[0].value
+                                    and tutor_subj.number == row[1].value
                                     and tutor_shift.day == ws.cell(row=1, column=day_col).value
                                     and tutor_shift.name == ws.cell(row=2, column=j+1).value):
                                     row[j].value = 'X'
